2a057d6fa0975e2e961fcf30e13b38eaaa12ad65
Deniss Celuiko <deniss.celuiko@gmail.com>
2023-06-16 03:04:24 +0300

    Minimize update lag

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->job(new UpdateCycle)
-                ->hourlyAt(15)
+                ->everyTenMinutes()
                 ->withoutOverlapping();
 
         $schedule->job(new ExportCycle)
