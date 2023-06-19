819f4d6da6aae006d9e634d05f745fcd1f237058
Deniss Celuiko <deniss.celuiko@gmail.com>
2023-06-19 16:58:45 +0300

    Partial for multi level export mappings

--- tests/Feature/Jobs/Exporter/XML/CompiledProductExportJobTest.php
+++ tests/Feature/Jobs/Exporter/XML/CompiledProductExportJobTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature\Jobs\Exporter\XML;
 
 use App\Jobs\Exporter\Xml\CompiledProductExportJob;
+use App\Models\CompiledProduct;
+use App\Models\Compiler;
 use App\Models\Export;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -34,6 +36,92 @@ class CompiledProductExportJobTest extends TestCase
         $expected =<<<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <products/>
+
+EOF;
+        $this->assertEquals($expected, Storage::disk('export')->get($filename));
+
+        Storage::disk('export')->delete($filename);
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function can_export_products()
+    {
+        $filename = 'xml-export.xml';
+
+        $fields = [
+            'ean' => 'string',
+            'price' => 'float',
+        ];
+
+        $compiler = Compiler::factory()
+                    ->fields($fields)
+                    ->has(CompiledProduct::factory()
+                        ->data([
+                            'ean' => '12345',
+                            'price' => 123.45,
+                        ])
+                        ->count(1)
+                    )
+                    ->has(Export::factory()
+                        ->xml()
+                    )
+                    ->create();
+
+        $job = new CompiledProductExportJob($compiler->exports->first(), $filename, 'export');
+
+        $job->handle();
+
+        $this->assertTrue(Storage::disk('export')->exists($filename));
+
+        $expected =<<<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<products><product><ean>12345</ean><price>123.45</price></product></products>
+
+EOF;
+        $this->assertEquals($expected, Storage::disk('export')->get($filename));
+
+        Storage::disk('export')->delete($filename);
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function can_handle_multi_level_mappings()
+    {
+        $filename = 'xml-export.xml';
+
+        $fields = [
+            'ean' => 'string',
+            'price' => 'float',
+        ];
+
+        $compiler = Compiler::factory()
+                    ->fields($fields)
+                    ->has(CompiledProduct::factory()
+                        ->data([
+                            'ean' => '12345',
+                            'price' => 123.45,
+                        ])
+                        ->count(1)
+                    )
+                    ->has(Export::factory()
+                        ->xml()
+                    )
+                    ->create();
+
+        $job = new CompiledProductExportJob($compiler->exports->first(), $filename, 'export');
+
+        $job->handle();
+
+        $this->assertTrue(Storage::disk('export')->exists($filename));
+
+        $expected =<<<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<products><product><ean>12345</ean><price>123.45</price></product></products>
 
 EOF;
         $this->assertEquals($expected, Storage::disk('export')->get($filename));
